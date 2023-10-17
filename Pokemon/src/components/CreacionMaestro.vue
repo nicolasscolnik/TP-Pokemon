4335c00ae3503f8a9d10fc580fef9d6817170a27
valentinArce <valentinarce5@gmail.com>
2023-10-17 20:11:41 -0300

    Primer commit - Valen

---
+++ Pokemon/src/components/CreacionMaestro.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref } from 'vue'
+import MaestroPokemon from './MaestroPokemon.vue'
+
+const MaestroPokemon = ref({
+  nombre: "",
+  Pokemon1: "",
+  Pokemon2: "",
+  Pokemon3: ""
+});
+
+const guardarMaestro = () => {
+
+  console.log(MaestroPokemon.value);
+
+  router.push('/BusquedaArena');
+};
+</script>
+
+<template>
+ 
+ <div>
+    <form @submit="guardarMaestro">
+      <label for="nombre">Nombre:</label>
+      <input type="text" v-model="MaestroPokemon.nombre" id="nombre" required>
+
+      <label for="pokemon1">Pokemon1:</label>
+      <input type="text" v-model="MaestroPokemon.Pokemon1" id="pokemon1" required>
+
+      <label for="pokemon2">Pokemon2:</label>
+      <input type="text" v-model="MaestroPokemon.Pokemon2" id="pokemon1" required>
+
+      <label for="pokemon3">Pokemon3:</label>
+      <input type="text" v-model="MaestroPokemon.Pokemon3" id="pokemon1" required>
+
+      <button type="submit">Guardar</button>
+
+      <router-link to="/BusquedaArena">Elegir Arena</router-link>
+    </form>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
