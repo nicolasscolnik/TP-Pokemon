54a7569eb01d90198f91ff9501c969e44189ab4b
nicolasscolnik <nicolasscolnik@gmail.com>
2023-10-19 18:23:40 -0300

    trabaje con el array cuando se manda pokemonaarena

--- Pokemon/src/components/ArenaDeBatalla.vue
+++ Pokemon/src/components/ArenaDeBatalla.vue
@@ -103,29 +103,72 @@ const settingLocal = () => {
     // console.log(datosPeleaActuales.value, esTurno1.value)
     if (jugador1 != null) {
         jugador2 = ref(storeMaestro);
+        pokemonEnArena2 = ref(storeMaestro.pokemonEnArena);
     } else {
         jugador1 = ref(storeMaestro);
+        pokemonEnArena1 = ref(storeMaestro.pokemonEnArena);
     }
 
     console.log("jugador1: " + jugador1 + "---" + "jugador2: " + jugador2)
     // console.log(store.nombre)
-    console.log(jugador1.pokemons)
-    console.log(jugador2.value)
+    // console.log(jugador1.pokemons)
+    // console.log(jugador2.value)
 }
 
-const enviarPokemonALaArena = (pokemon) => {  
-    debugger
-    const pokeAAsignar = pokemon;
-    console.log(pokemon)
-    console.log(jugador2.pokemons)
-    if (jugador2) {
-        jugador2.value.pokemons.push(pokemonEnArena2.value); // me tira error al renderizar el atributo nombre(creo que del pokemon insertado)
-        pokemonEnArena2.value = jugador2.pokemons.pop(pokemon);
-    } else {
-        jugador1.pokemons.push(pokemonEnArena1.value);
-        pokemonEnArena1 = ref(jugador1.value.pokemons.pop(pokemon));
+// const enviarPokemonALaArena = (pokemon) => {
+//     debugger
+//     const pokeAAsignar = pokemon;
+//     // console.log(pokemon)
+//     // console.log(pokemonEnArena2)
+//     if (jugador2) {
+//         jugador2.value.pokemons.push(pokemonEnArena2.value);
+//         pokemonEnArena2.value = ref(jugador2.value.pokemons.pop(pokeAAsignar));
+//         console.log(jugador2.value.pokemons)
+//         console.log(pokemonEnArena2)
+//     } else {
+//         jugador1.pokemons.push(pokemonEnArena1.value);
+//         pokemonEnArena1 = ref(jugador1.value.pokemons.pop(pokemon));
+//     }
+// };
+
+// const enviarPokemonALaArena = (pokemon) => {
+//     debugger;
+//     const pokeAAsignar = pokemon;
+
+//     if (jugador2.value) {
+//         // Si jugador2.value existe
+//         jugador2.value.pokemons.push(pokemonEnArena2.value); // Agrega pokemonEnArena2 al array de pokemons de jugador2
+//         pokemonEnArena2.value = pokeAAsignar; // Asigna el nuevo valor a pokemonEnArena2
+//         console.log(jugador2.value.pokemons);
+//         console.log(pokemonEnArena2.value);
+//     } else {
+//         // Si jugador2.value no existe, asume que jugador1 es la referencia correcta
+//         jugador1.pokemons.push(pokemonEnArena1.value);
+//         pokemonEnArena1.value = pokeAAsignar;
+//     }
+// };
+
+const enviarPokemonALaArena = (pokemon) => {
+  debugger;
+  const pokeAAsignar = pokemon;
+  
+  if (jugador2.value) {
+    // Si jugador2.value existe
+    const indice = jugador2.value.pokemons.indexOf(pokeAAsignar);
+    if (indice !== -1) {
+      jugador2.value.pokemons.splice(indice, 1); // Elimina el Pokémon del array de pokemons de jugador2
+      storeMaestro.pokemonEnArena.value = ref(pokeAAsignar); // Asigna el nuevo valor a pokemonEnArena2
     }
-    console.log(jugador2.pokemons)
+    console.log(jugador2.value.pokemons);
+    console.log(storeMaestro.pokemonEnArena.value);
+  } else {
+    // Si jugador2.value no existe, asume que jugador1 es la referencia correcta
+    const indice = jugador1.value.pokemons.indexOf(pokeAAsignar);
+    if (indice !== -1) {
+      jugador1.value.pokemons.splice(indice, 1); // Elimina el Pokémon del array de pokemons de jugador1
+      storeMaestro.pokemonEnArena.value = ref(pokeAAsignar);
+    }
+  }
 };
 
 
@@ -146,8 +189,9 @@ onMounted(() => {
         <MaestroPokemon @horadeluchar="enviarPokemonALaArena($event)" @lastimar="atacar(1)" @curar="curar(1)" nombre="Ash"
             :pokemons="storeMaestro.pokemons" :tuTurno="esTurno1" :pokemonEnArena="storeMaestro.pokemonEnArena">
             Luchador 1</MaestroPokemon>
-        <MaestroPokemon @horadeluchar="enviarPokemonALaArena($event)" @lastimar="atacar(2)" @curar="curar(2)" nombre="La otra"
-            :pokemons="storeMaestro.pokemons" :tuTurno="!esTurno1" :pokemonEnArena="storeMaestro.pokemonEnArena">
+        <MaestroPokemon @horadeluchar="enviarPokemonALaArena($event)" @lastimar="atacar(2)" @curar="curar(2)"
+            nombre="La otra" :pokemons="storeMaestro.pokemons" :tuTurno="!esTurno1"
+            :pokemonEnArena="storeMaestro.pokemonEnArena">
             Luchador 2</MaestroPokemon>
     </div>
 </template>
