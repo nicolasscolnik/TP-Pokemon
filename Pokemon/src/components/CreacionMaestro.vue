169a83b2fa284dbce3b3dd2a5f5c821b22e9ee09
Matias Damian Belli <matiasdbelli98@gmail.com>
2023-11-15 15:58:07 -0300

    se agrego css en CreacionMaestro

--- Pokemon/src/components/CreacionMaestro.vue
+++ Pokemon/src/components/CreacionMaestro.vue
@@ -1,16 +1,14 @@
+
 <script setup>
 import { ref } from 'vue'
 import MaestroPokemon from './MaestroPokemon.vue'
 import { useStoreMaestroPokemon } from '/stores/storeMaestroPokemon'
-
 const storeMaestro = useStoreMaestroPokemon();
-
 const maestroPokemon = ref({
   nombre: "",
   tuTurno: true,
   pokemons: []
 });
-
 function crearPokemon(nombreAPI, vidaAPI, ataqueAPI, defensaAPI, fotoAPI) {
   let poke = {
     nombre: nombreAPI,
@@ -21,20 +19,16 @@ function crearPokemon(nombreAPI, vidaAPI, ataqueAPI, defensaAPI, fotoAPI) {
   }
   return poke;
 }
-
 const guardarMaestro = async () => {
   await cargarPokemones();
   console.log(maestroPokemon.value.pokemons)
   storeMaestro.setter(maestroPokemon)
   console.log("store: " + storeMaestro.value)
 };
-
 const cargarPokemones = async () => {
   try {
     const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
-
     const getRandomPokemonId = () => Math.floor(Math.random() * 151) + 1;
-
     const pokemons = [];
     for (let i = 0; i < 4; i++) {
       const randomPokemonId = getRandomPokemonId();
@@ -54,23 +48,88 @@ const cargarPokemones = async () => {
     console.error("Error al obtener Pokémon:", error);
   }
 };
-
 </script>
-
 <template>
-  <div>
-
-    <form>
+<div class="gradient-background">
+  <div class="logo-container">
+    <img class="logo" src="./International_Pokémon_logo.svg.png" alt="" srcset="">
+  </div>
+  <div class="container-formulario">
+    <form class="formulario">
       <label for="nombre">Nombre:</label>
+      <br>
       <input type="text" v-model="maestroPokemon.nombre" id="nombre" required>
-
-      
-
+      <br>
       <router-link to="/BusquedaArena"><button @click="guardarMaestro">Guardar</button></router-link>
     </form>
-    <button @click="guardarMaestro">pruebaLocal</button>
+    <br>
+    <br>
   </div>
-  <div v-if="storeMaestro!==null">{{ storeMaestro }}</div>
+  <button @click="guardarMaestro">pruebaLocal</button>
+  <div v-if="storeMaestro !== null">{{ storeMaestro }}</div>
+</div>
 </template>
+<style scoped>
+body{
+  margin: 0px;
+  padding: 0px;
+  width: 100%;
+  height: 100%;
+}
+.gradient-background {
+  background: linear-gradient(300deg,deepskyblue,darkviolet,blue);
+  background-size: 180% 180%;
+  animation: gradient-animation 18s ease infinite;
+}
+@keyframes gradient-animation {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
 
-<style scoped></style>
+.logo-container {
+  width: 100%;
+  height: 100%;
+  border: 2px solid black;
+  margin-bottom: 10px;
+}
+.logo {
+  width: 800px;
+  height: 275px;
+  margin: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.container-formulario {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid black;
+  padding: 15px;
+}
+.formulario {
+  display: inline-block;
+  justify-content: center;
+  align-items: center;
+  width: 400px;
+  height: 150px;
+  border: 2px solid rgb(247, 243, 9);
+  border-radius: 10px;
+  text-align: center;
+  background: white;
+}
+.formulario label{
+  margin-top: 5px;
+}
+.formulario input{
+  width: 250px;
+  margin: 15px;
+}
+</style>
